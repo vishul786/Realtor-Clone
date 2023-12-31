4102f0db334306c3b5ef70b11130dac36d216c89
vishul786 <vishulk786@gmail.com>
2023-12-31 12:28:40 +0530

    Create Header of Page

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import ForgotPassword from "./pages/ForgotPassword";
 import Offers from "./pages/Offers";
+import Header from "./components/Header";
 function App() {
   return (
     <>
       <Router>
+        <Header>
         <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path="/profile" element={<Profile/>}/>
@@ -17,6 +19,7 @@ function App() {
           <Route path="/forgot-password" element={<ForgotPassword/>}/>
           <Route path="/offers" element={<Offers/>}/>
         </Routes>
+        </Header>
       </Router>
     </>
   );
