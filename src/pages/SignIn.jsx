6506a277552e85fe1931a6c51f01b5a33b99c56f
vishul786 <vishulk786@gmail.com>
2024-01-12 20:03:25 +0530

    Complete Sign In Functionality

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,7 +1,10 @@
 import { useState } from 'react'
 import { AiFillEyeInvisible, AiFillEye } from "react-icons/ai"
-import {Link} from "react-router-dom"
+import {Link, useNavigate} from "react-router-dom"
 import OAuth from '../components/OAuth';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import { toast } from 'react-toastify';
+
 
 
 export default function SignIn() {
@@ -13,12 +16,29 @@ export default function SignIn() {
   });
 
   const {email, password} = formData
+  const navigate = useNavigate()
   function onChange(e) {
     // console.log(e.target.value)
     setFormData((prevState) => ({
       ...prevState,
       [e.target.id]: e.target.value
     }));
+  }
+
+  async function onSubmit(e){
+    e.preventDefault()
+    try {
+      const auth = getAuth()
+      const userCredential = await
+      signInWithEmailAndPassword(auth,email,password)
+      if(userCredential.user){
+        navigate('/')
+      }
+      
+    } catch (error) {
+      toast.error("Bad user Credential!!")
+      
+    }
   }
 
   return (
@@ -34,7 +54,7 @@ export default function SignIn() {
           />
         </div>
         <div className='w-full md:w-[67%] lg:w-[40%] lg:ml-20'>
-          <form >
+          <form  onSubmit={onSubmit}>
             <input
               type="email"
               id="email"
